beafff8ab01712297d016e8251236b9ca6fdff1a
Jude Daryl Clarino <jclarino@savvysherpa.com>
2018-10-21 14:36:24 +0800

    Removed initialize

--- thunder.auth/Startup.cs
+++ thunder.auth/Startup.cs
@@ -79,7 +79,7 @@ namespace thunder.auth
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            InitializeDatabase(app);
+            // InitializeDatabase(app);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
